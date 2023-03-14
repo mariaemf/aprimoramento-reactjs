2f763d3c117a120f4e9ada731580d006bce065af
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-14 17:27:59 -0300

    revisando css modules, suas diferenças do css tradicional e os beneficios que o mesmo fornece para o desenvolvimento dos apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 /*jsx = javascript + xml = xml = arquivo q contem html dentro dele */
 import { Fragment } from "react";
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css";
 
 export function App() {
   return (
-    <>
+    <Fragment>
+      <Header />
+
       <Post
         author="Maria Freitas"
         content="estou estudando para aprimorar meu desenvolvimento"
       />
       <Post author="Sérgio Luiz" content="Nova atualização de emprego" />
-    </>
+    </Fragment>
   );
 }
 
