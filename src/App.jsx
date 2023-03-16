fcdc0eb68a567c721662c941fe828e436fb7f9f6
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-16 18:07:01 -0300

    feat: teste de commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@
 import { Fragment } from "react";
 import { Header } from "./components/Header";
 import { Post } from "./Post";
-
+import styles from "./App.module.css";
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <Fragment>
       <Header />
-
-      <Post
-        author="Maria Freitas"
-        content="estou estudando para aprimorar meu desenvolvimento"
-      />
-      <Post author="Sérgio Luiz" content="Nova atualização de emprego" />
+      <div className={styles.wrapper}>
+        {/*a tag aside e usada para definir conteudo lateral que e relaionado ao conteudo principal d apagina, mas nao faz parde dele*/}
+        <Sidebar />
+        {/*tag main e usada para definir o conteudo
+        principal da pagina -> normalmente sendo o mais 
+        importante apenas teste de commit */}
+        <main>
+          {" "}
+          <Post
+            author="Maria Freitas"
+            content="estou estudando para aprimorar meu desenvolvimento"
+          />
+          <Post author="Sérgio Luiz" content="Nova atualização de emprego" />
+        </main>
+      </div>
     </Fragment>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <>
+      <aside className={styles.Sidebar}></aside>
+    </>
+  );
+}
