6e75e3f7b3fd175b0398ec3402ac2a889d60c68b
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-17 20:30:27 -0300

    continuacao de desenvolvimento de sidebar, finalizacao da estilização do comentendo e instalacao de biblioteca para icones

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import styles from "./sidebar.module.css";
+import { PencilSimpleLine } from "@phosphor-icons/react";
 
 export function Sidebar() {
   return (
-    <>
-      <aside className={styles.sidebar}>
-        {/*Endereço de imagem copiado e colado com algumas alteracaoes no tamanho total e qualidade da mesma*/}
-        <img
-          classname={styles.cover}
-          src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcG1lbnR8ZW58MHx8MHx8&w=500&q=50"
-        />
-        {/* Essa div será o perfil do usuário */}
-        <div className={styles.profile}>
-          <strong>Maria Freitas</strong>
-          <span>Web Developer</span>
-        </div>
+    <aside className={styles.sidebar}>
+      {/*Endereço de imagem copiado e colado com algumas alteracaoes no tamanho total e qualidade da mesma*/}
+      <img
+        classname={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      {/* Essa div será o perfil do usuário */}
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/mariaemf.png" />
 
-        <footer>
-          {/*O botao esta sendo usando como link 
+        <strong>Maria Freitas</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        {/*O botao esta sendo usando como link 
           pq aparentemente nao parece um botao (pelo designer) 
           demonstrando que ao clikar o usuario sera 
           redirecionado para outra sessao/pag*/}
-          <a href="#">Editar seu perfil</a>
-        </footer>
-      </aside>
-    </>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
   );
 }
