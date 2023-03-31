94b6e8535be8d62a6ceca9990857783c625cde4c
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-30 21:46:16 -0300

    buscando saber mais sobre key e resolucionando problemas no console do mesmo

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -110,13 +110,15 @@ spread (ele le o valor da variavel e copia os mesmos)
         -> se a linha for do tipo link, a mesma sera um paragrafo mas de lnk 
         
         */}
+        {/*a key sempre deve ser colocada no primeiro 
+        elemento de retorno de um map  */}
         <div className={styles.content}>
           {content.map((line) => {
             if (line.type === "paragraph") {
-              return <p>{line.content}</p>;
+              return <p key={line.content}>{line.content}</p>;
             } else if (line.type === "link") {
               return (
-                <p>
+                <p key={line.content}>
                   <a href="">{line.content}</a>
                 </p>
               );
@@ -150,7 +152,7 @@ spread (ele le o valor da variavel e copia os mesmos)
 
         <div className={styles.commentList}>
           {comments.map((comment) => {
-            return <Comment content={comment} />;
+            return <Comment key={comment} content={comment} />;
           })}
         </div>
       </article>
